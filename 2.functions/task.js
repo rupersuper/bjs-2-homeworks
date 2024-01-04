145277afef73d38b257b19e4914023bfce2d6a1c
rupersuper <rupersuper@mail.ru>
2024-01-05 02:30:39 +0600

    chaged task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,7 +2,6 @@ function getArrayParams(...arr) {
   let min = arr[0];
   let max = arr[0];
   let sum = 0;
-
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > max) {
       max = arr[i];
@@ -12,9 +11,8 @@ function getArrayParams(...arr) {
     }
     sum += arr[i];
   }
-
+  
   let avg = Number((sum / arr.length).toFixed(2));
-
   return { min: min, max: max, avg: avg };
 }
 
@@ -26,7 +24,6 @@ function summElementsWorker(...arr) {
   sum = arr.reduce((accumulator, currentNumber) => {
     return accumulator + currentNumber;
   }, 0);
-
   return sum;
 }
 
@@ -41,7 +38,6 @@ function differenceMaxMinWorker(...arr) {
 function differenceEvenOddWorker(...arr) {
   let sumEvenElement = 0;
   let sumOddElement = 0;
-
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 === 0) {
       sumEvenElement += arr[i];
@@ -55,7 +51,6 @@ function differenceEvenOddWorker(...arr) {
 function averageEvenElementsWorker(...arr) {
   let sumEvenElement = 0;
   let countEvenElement = 0;
-
   if (arr.length === 0) {
     return 0;
   } else {
@@ -66,13 +61,11 @@ function averageEvenElementsWorker(...arr) {
       }
     }
   }
-
   return sumEvenElement / countEvenElement;
 }
 
 function makeWork(arrOfArr, func) {
   let maxWorkerResult = -Infinity;
-
   for (let i = 0; i < arrOfArr.length; i++) {
     const arr = arrOfArr[i];
     const result = func(...arr);
