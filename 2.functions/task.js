9a1d50da6072e21370868b543b0c243ff247fe70
rupersuper <rupersuper@mail.ru>
2024-01-04 21:54:39 +0600

    Functions задача 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,27 @@
 function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }  
+  let avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
-function summElementsWorker(...arr) {
+function summElementsWorker(...arr) {}
 
-}
+function differenceMaxMinWorker(...arr) {}
 
-function differenceMaxMinWorker(...arr) {
+function differenceEvenOddWorker(...arr) {}
 
-}
+function averageEvenElementsWorker(...arr) {}
 
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
-}
+function makeWork(arrOfArr, func) {}
