10712b9904156586459f906ab1aa644f84d3448a
rupersuper <rupersuper@mail.ru>
2024-01-20 20:18:34 +0600

    task 3.arrays completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
-}
+	if (arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index])) {
+	  return true;
+	}
+	return false;
+  }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+  function getUsersNamesInAgeRange(users, gender) {
+    let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, arr) => {
+        acc += item;
+        if (index === arr.length - 1) {
+            return acc / arr.length;
+        }
+      return acc;
+    }, 0)
+  return result;
 }
