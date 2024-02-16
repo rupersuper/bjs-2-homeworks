cafc5531e967b81056f5ce57c2e88ba15252250c
rupersuper <rupersuper@mail.ru>
2024-02-17 00:32:10 +0600

    task 6.exception-closure completed

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,52 @@
-﻿
+﻿function parseCount(value) {
+  if (isNaN(Number.parseFloat(value))) {
+    throw new Error("Невалидное значение");
+  }
+  return Number.parseFloat(value);
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  } finally {
+  }
+}
+
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if (a + b < c || a + c < b || b + c < a) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  get area() {
+    const p = this.perimeter / 2;
+    return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(
+      3
+    );
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+    };
+  }
+}
